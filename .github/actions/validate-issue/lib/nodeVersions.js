f02ffcd4f978f8bfd55f537ccfeb486031e16a3e
mshanemc <shane.mclaughlin@salesforce.com>
2024-02-12 15:49:30 -0600

    chore: compiles

--- .github/actions/validate-issue/lib/nodeVersions.js
+++ .github/actions/validate-issue/lib/nodeVersions.js
@@ -2,18 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.isValidVersion = void 0;
 const isValidVersion = (currentDate) => async (version) => {
-  const formattedVersion = `v${version}`;
-  const resp = await (
-    await fetch(
-      "https://raw.githubusercontent.com/nodejs/Release/main/schedule.json"
-    )
-  ).json();
-  console.log(resp);
-  return (
-    formattedVersion in resp &&
-    currentDate >= resp[formattedVersion].start &&
-    currentDate <= resp[formattedVersion].end
-  );
+    const formattedVersion = `v${version}`;
+    const resp = (await (await fetch("https://raw.githubusercontent.com/nodejs/Release/main/schedule.json")).json());
+    return (formattedVersion in resp &&
+        currentDate >= new Date(resp[formattedVersion].start) &&
+        currentDate <= new Date(resp[formattedVersion].end));
 };
 exports.isValidVersion = isValidVersion;
 //# sourceMappingURL=nodeVersions.js.map
