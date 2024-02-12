51e1e81a961b11ce790402453922eaf336929428
Eric Willhoit <ewillhoit@salesforce.com>
2024-02-12 15:00:26 -0600

    chore: global regex and build

--- .github/actions/validate-issue/lib/nodeVersions.js
+++ .github/actions/validate-issue/lib/nodeVersions.js
@@ -6,11 +6,12 @@ exports.isValidVersion = void 0;
  * even (LTS) versions start in mid april and end the following 4/30
  * */
 const versionMap = new Map([
-    ["18", { start: new Date("2023-04-15"), end: new Date("2025-04-30") }],
-    ["20", { start: new Date("2023-04-15"), end: new Date("2026-04-30") }],
-    ["21", { start: new Date("2023-10-15"), end: new Date("2024-06-01") }],
-    ["22", { start: new Date("2024-04-15"), end: new Date("2027-06-30") }],
+    ["18", { start: new Date("2022-04-19"), end: new Date("2025-04-30") }],
+    ["20", { start: new Date("2023-04-18"), end: new Date("2026-04-30") }],
+    ["21", { start: new Date("2023-10-17"), end: new Date("2024-06-01") }],
+    ["22", { start: new Date("2024-04-23"), end: new Date("2027-04-30") }],
     ["23", { start: new Date("2024-10-15"), end: new Date("2025-06-01") }],
+    ["24", { start: new Date("2025-04-22"), end: new Date("2028-04-30") }],
 ]);
 const isValidVersion = (currentDate) => (version) => versionMap.has(version) &&
     currentDate >= versionMap.get(version).start &&
